9bd6fa4c65817297ffc6867a7384a772198bf9f1
Samu <samu.uunonen@student.hamk.fi>
2022-07-05 10:42:16 +0300

    Add feature map
    WIP Added map to add feature, to show location.  Atm not showing the location on the map, but map is visible.,

--- src/components/Coords.jsx
+++ src/components/Coords.jsx
@@ -1,6 +1,12 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
+import ReactMapboxGl, { Layer, Feature, Marker } from "react-mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const Coords = ({ coords, setCoords }) => {
+  const Map = ReactMapboxGl({
+    accessToken: process.env.REACT_APP_MAPBOX_API_KEY,
+  });
+
   const getLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.watchPosition((position) => {
@@ -20,10 +26,35 @@ const Coords = ({ coords, setCoords }) => {
         </Button>
       </Row>
       {coords && (
-        <Row>
-          <Col>lat: {coords.latitude}</Col>
-          <Col>long: {coords.longitude}</Col>
-        </Row>
+        <>
+          <Row>
+            <Col>lat: {coords.latitude}</Col>
+            <Col>long: {coords.longitude}</Col>
+          </Row>
+          <Row>
+            <Map
+              style="mapbox://styles/mapbox/satellite-v9"
+              containerStyle={{
+                height: "50vh",
+                width: "100vw",
+              }}
+              center={[coords.longitude, coords.latitude]}
+              zoom={[10]}
+            >
+              <Layer
+                type="symbol"
+                id="marker"
+              
+              >
+                <Feature coordinates={[coords.longitude, coords.latitude]} />
+              </Layer>
+              <Marker
+                coordinater={[coords.longitude, coords.latitude]}
+                anchor="bottom"
+              ></Marker>
+            </Map>
+          </Row>
+        </>
       )}
     </Container>
   );
