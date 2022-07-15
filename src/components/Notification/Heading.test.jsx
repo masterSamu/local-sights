efd0e70141c93941c8aa5fdaa93e6705ef0a8f4b
Samu <samu.uunonen@student.hamk.fi>
2022-07-15 15:45:40 +0300

    Heading unit tests
    Tests for heading types

--- src/components/Notification/Heading.test.jsx
+++ src/components/Notification/Heading.test.jsx
@@ -1,4 +1,4 @@
-import { screen, re } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { renderWithProviders } from "../../utils/test_utils";
 import "@testing-library/jest-dom/extend-expect";
 import Heading from ".";
@@ -7,15 +7,68 @@ describe("Notification heading tests", () => {
   test("Success text is rendering when type is success", () => {
     renderWithProviders(<Heading type="success" />, {
       preloadedState: {
-        notifications: [{
-          id: "123asd",
-          type: "success",
-          message: "Hello world",
-        }],
+        notifications: [
+          {
+            id: "123asd",
+            type: "success",
+            message: "Hello world",
+          },
+        ],
       },
     });
 
     const component = screen.getByText("Success");
+    expect(component).toBeInTheDocument();
+  });
+
+  test("Error text is rendering when type is error", () => {
+    renderWithProviders(<Heading type="error" />, {
+      preloadedState: {
+        notifications: [
+          {
+            id: "123asd",
+            type: "error",
+            message: "Hello world",
+          },
+        ],
+      },
+    });
+
+    const component = screen.getByText("Error");
+    expect(component).toBeInTheDocument();
+  });
+
+  test("Warning text is rendering when type is warning", () => {
+    renderWithProviders(<Heading type="warning" />, {
+      preloadedState: {
+        notifications: [
+          {
+            id: "123asd",
+            type: "warning",
+            message: "Hello world",
+          },
+        ],
+      },
+    });
+
+    const component = screen.getByText("Warning");
+    expect(component).toBeInTheDocument();
+  });
+
+  test("Info text is rendering when type is info", () => {
+    renderWithProviders(<Heading type="info" />, {
+      preloadedState: {
+        notifications: [
+          {
+            id: "123asd",
+            type: "info",
+            message: "Hello world",
+          },
+        ],
+      },
+    });
+
+    const component = screen.getByText("Info");
     expect(component).toBeInTheDocument();
   });
 });
