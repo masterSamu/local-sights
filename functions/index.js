d8dd331dfbc0bbd8e89b6a3d2937bdfe8ac467e0
Samu <samu.uunonen@student.hamk.fi>
2022-07-23 19:31:47 +0300

    App check
    Added app check to Cloud function in production mode.

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,32 @@
 const functions = require("firebase-functions");
 
 /**
- * Count likes for sight when sight is updated, and update 
+ * Count likes for sight when sight is updated, and update
  * likes.positive and .negative values on database
  */
-exports.updateLikeCount = functions.firestore
-  .document("sights/{docId}")
+exports.updateLikeCount = functions
+  .runWith({
+    allowInvalidAppCheckToken: false,
+  })
+  .firestore.document("sights/{docId}")
   .onUpdate((change, context) => {
+    // App check
+    if (process.env.FUNCTIONS_EMULATOR === false) {
+      if (context.app === undefined) {
+        throw new functions.https.HttpsError(
+          "failed-precondition",
+          "The function must be called from an App Check verified app."
+        );
+      }
+    }
+
     const data = change.after.data().likes;
     const previousData = change.before.data().likes;
+
     if (data.likedUsers.length !== previousData.likedUsers.length) {
       let positive = 0;
       let negative = 0;
+
       data.likedUsers.forEach((user) => {
         switch (user.type) {
           case "positive":
@@ -24,6 +39,8 @@ exports.updateLikeCount = functions.firestore
             break;
         }
       });
+
+      // Update data in firestore database
       return change.after.ref.set(
         { likes: { ...data.likes, positive, negative } },
         { merge: true }
