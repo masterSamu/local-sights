fc8c3f0401f0d7987133955263bcfed7c53bca15
Samu <samu.uunonen@student.hamk.fi>
2022-07-31 20:55:46 +0300

    Fixed navbar style
    Navbar was not working responsively.

--- src/components/Navbar/Navigationbar.jsx
+++ src/components/Navbar/Navigationbar.jsx
@@ -7,15 +7,19 @@ import ProtectedLinks from "./ProtectedLinks";
 const Navigationbar = ({ logOut }) => {
   const user = useSelector((state) => state.user);
   return (
-    <Navbar bg="light">
+    <Navbar bg="light" collapseOnSelect expand="sm">
       <Container>
         <Navbar.Brand as={Link} to="/">
           LocalSights
         </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            {user ? <ProtectedLinks logOut={logOut} /> : <PublicLinks />}
+          </Nav>
+        </Navbar.Collapse>
       </Container>
-      <Nav className="me-auto">
-        {user ? <ProtectedLinks logOut={logOut} /> : <PublicLinks />}
-      </Nav>
     </Navbar>
   );
 };
